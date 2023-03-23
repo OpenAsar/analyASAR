7f52d13b855d35bc95f04b7de1bfa796b825be09
CanadaHonk <oj@oojmed.com>
2023-03-23 23:10:58 +0000

    0.2.0: fancy styling

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
-const asar = require('asar');
+const asar = require('@electron/asar');
+
+const bold = s => `\u001b[1m${s}\u001b[0m`;
+const underline = s => `\u001b[4m${s}\u001b[0m`;
+const color = (r, g, b, s) => `\u001b[38;2;${r};${g};${b}m${s}\u001b[0m`;
+const severity = (percent, s) => color(Math.max(60, Math.floor(percent / maxPercent * 255)), 60, 60, s);
 
 const file = process.argv[2];
 
-console.log(file);
+console.log(bold(underline(file)));
 
 const fileSize = fs.statSync(file).size;
 
@@ -43,9 +48,12 @@ for (const f in files) {
   files[f] = (sum / fileSize) * 100;
 }
 
+const filenames = Object.keys(files).sort((a, b) => files[b] - files[a]);
+const maxPercent = files[filenames[0]];
+
 // console.log(Object.keys(files).sort((a, b) => files[b] - files[a]).reduce((acc, x) => { acc[x] = files[x].toFixed(2) + '%'; return acc; }, {}));
-for (const f of Object.keys(files).sort((a, b) => files[b] - files[a])) {
-  console.log(f, ' '.repeat(30 - f.length), files[f].toFixed(2) + '%', `${files[f] < 10 ? ' ' : ''}| ${(oldFiles[f][2] / 1024).toFixed(2)} kb, ${(oldFiles[f][0] / 1024).toFixed(2)} kb header`);
+for (const f of filenames) {
+  console.log(severity(files[f], bold(f)), ' '.repeat(30 - f.length + (files[f] < 10 ? 1 : 0)), files[f].toFixed(2) + '%', `${color(80, 80, 80, '║')} ${oldFiles[f][2] / 1024 < 10 ? ' ' : ''}${(oldFiles[f][2] / 1024).toFixed(2)} kb` + color(80, 80, 80, ` + ${(oldFiles[f][0] / 1024).toFixed(2)} kb header`));
 }
 
 console.log();
